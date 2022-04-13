5aea937e13d280431bb0e7492223c12c05952425
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2022-04-14 01:13:34 +0200

    add revalidate

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -11,6 +11,10 @@ const typeDefs = gql`
   type Query {
     getUser: User
   }
+
+  type Mutation {
+    revalidate(slug: ID!): Boolean
+  }
 `;
 
 const resolvers = {
@@ -23,6 +27,14 @@ const resolvers = {
       };
     },
   },
+  Mutation: {
+    revalidate: async (_, { slug }, ctx) => {
+      console.log('slug', slug)
+      await ctx.revalidate('/')
+      await ctx.revalidate(`/blog/${slug}`)
+      return true
+    }
+  }
 };
 
 const apolloServer = new ApolloServer({
