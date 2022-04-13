fea8068510dd4906e598790f181585b5699bbe83
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2022-04-13 23:18:00 +0200

    remove apos

--- components/Home.js
+++ components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
           Clint Briley
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          I&apos;m a Full Stack Developer.
+          I'm a Full Stack Developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-2xl">
           Elit non sit duis aliquip. Voluptate commodo ea aliquip eu nostrud
