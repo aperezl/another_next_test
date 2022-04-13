0a82c3180d3eac2af026eae7891097207803423c
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2022-04-14 00:35:37 +0200

    add prisma support

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,5 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
-import { signOut, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Login = () => {
   const router = useRouter();
@@ -8,14 +7,18 @@ const Login = () => {
   const { data: session, status } = useSession();
 
   if (status === "loading") return <div>loading</div>;
-  if (!session) return <div>No session, please Login</div>;
+  if (!session) return (
+    <div>
+      No session, please Login
+      <button onClick={() => signIn() }>Sign In</button>
+    </div>
+  )
 
-  // ¿Se podría eliminar éste if?
-  // if (session) {
   return (
     <p>
-      {session.user.name} ({session.user.email})
+      {session.user.name} ({session.user.email}) <button onClick={() => signOut() }>Sign Out</button>
     </p>
   );
-  // }
 };
+
+export default Login
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import { getPost } from "../lib/mdx";
+import Login from '../components/Login'
 import Link from 'next/link'
-// import prisma from "../lib/prisma";
 
 export default function Home({ posts }) {
   return (
     <>
+      <Login />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       {posts.map((f) => (
         <li key={f.slug}>
@@ -21,24 +22,6 @@ export default function Home({ posts }) {
 }
 
 export const getStaticProps = async () => {
-  const posts = await getPost()
-  console.log(posts)
-  return {
-    props: {
-      posts
-    }
-  }
-}
-
-
-// export const getStaticProps = async () => {
-//   const feed = await prisma.post.findMany({
-//     where: { published: true },
-//     include: {
-//       author: {
-//         select: { name: true },
-//       },
-//     },
-//   });
-//   return { props: { feed }, revalidate: false };
-// };
+  const posts = await getPost();
+  return { props: { posts }, revalidate: false };
+};
