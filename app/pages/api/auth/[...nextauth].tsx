09105ba53f63e59ecabc376baf2b009b04b3fae5
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2022-04-21 16:01:09 +0200

    chore: remove prisma from app

--- app/pages/api/auth/[...nextauth].tsx
+++ app/pages/api/auth/[...nextauth].tsx
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GitHubProvider from "next-auth/providers/github";
-import prisma from "../../../lib/prisma";
 
 const authHandler = (req, res) => NextAuth(req, res, options);
 export default authHandler;
@@ -13,6 +11,5 @@ const options = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
 };
