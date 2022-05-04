ec02249bd93a646f58fc68f831d99db5e7f93c94
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2022-05-04 20:47:20 +0200

    chore: add new design

---
+++ app/components/v2/BlogHome.js
@@ -0,0 +1,20 @@
+import Article from "./Article";
+
+export default function BlogHome () {
+  return (
+    <>
+      <div className="mb-10 md:mb-16">
+        <h2 className="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">Blog</h2>
+
+        <p className="max-w-screen-md text-gray-500 md:text-lg text-center mx-auto">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated.</p>
+      </div>
+
+      <div className="grid sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8 sm:gap-12 xl:gap-16">
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+      </div>
+    </>
+  )
+}
---
+++ app/components/v2/Navbar.js
@@ -0,0 +1,24 @@
+export default function Navbar () {
+  return (
+    <header className="absotule flex justify-between items-center py-4">
+      <a href="/" className="inline-flex items-center text-black-800 text-2xl md:text-3xl font-bold gap-2.5" aria-label="logo">
+        { "{ antonio: 'perez' } " }
+      </a>
+
+      <nav className="hidden lg:flex gap-12">
+        <a href="#" className="text-indigo-500 text-lg font-semibold">Home</a>
+        <a href="#" className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">Features</a>
+        <a href="#" className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">Pricing</a>
+        <a href="#" className="text-gray-600 hover:text-indigo-500 active:text-indigo-700 text-lg font-semibold transition duration-100">About</a>
+      </nav>
+
+      <a href="#" className="hidden lg:inline-block bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-8 py-3">Contact Sales</a>
+
+      <button type="button" className="inline-flex items-center lg:hidden bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold rounded-lg gap-2 px-2.5 py-2">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
+        </svg>
+      </button>
+    </header>
+  )
+}
