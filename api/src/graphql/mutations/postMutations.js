41d40b9a1af6943177cc0edc239c7e2970817ee9
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2022-05-06 12:58:00 +0200

    refactor: move mutations to their own file

---
+++ api/src/graphql/mutations/postMutations.js
@@ -0,0 +1,15 @@
+import { GraphQLString } from "graphql";
+import { Post } from "../types";
+
+export const makecreatePost = ({ postRepository }) => ({
+  type: Post,
+  description: 'Create a new post',
+  args: {
+    title: { type: GraphQLString },
+    body: { type: GraphQLString }
+  },
+  resolve: async (_, { title, body }, { user, auth }) => {
+    const authorId = auth.user._id
+    return await postRepository.create({ title, body, authorId })
+  }
+})
