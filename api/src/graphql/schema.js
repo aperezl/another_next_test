d64a3086bfab06dd7c008a6d4dfc0eac14abadae
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2022-05-07 17:11:21 +0200

    refactor: add createPostController

--- api/src/graphql/schema.js
+++ api/src/graphql/schema.js
@@ -4,6 +4,25 @@ import { makeLogin, makeRegister } from './mutations/auth'
 import { makeCreatePost } from './mutations/post'
 import { createJWT } from '../util/auth'
 
+// Utils
+import { validator } from '../util/validator'
+
+// Repositories
+import userMongoRepository from '../repositories/userMongoRepository'
+import postMongoRepository from '../repositories/postMongoRepository'
+
+// Controllers
+import { makeLoginController, makeRegisterController } from '../controllers/auth'
+import { makeCreatePostController } from '../controllers/post'
+
+// Compose LoginController
+const userRepository = userMongoRepository()
+const postRepository = postMongoRepository()
+
+// Compose Controllers
+const loginController = makeLoginController({ userRepository, createJWT })
+const registerController = makeRegisterController({ userRepository, createJWT, validator})
+const createPostController = makeCreatePostController({ postRepository })
 export default function makeSchema ({ userRepository, postRepository }) {
   const query = new GraphQLObjectType({
     name: 'QueryType',
@@ -18,9 +37,9 @@ export default function makeSchema ({ userRepository, postRepository }) {
     name: 'MutationType',
     description: 'The root mutation type',
     fields: {
-      register: makeRegister({ userRepository, createJWT }),
-      login: makeLogin({ userRepository, createJWT }),
-      createPost: makeCreatePost({ postRepository })
+      register: makeRegister({ registerController }),
+      login: makeLogin({ loginController }),
+      createPost: makeCreatePost({ createPostController })
     }
   })
 
